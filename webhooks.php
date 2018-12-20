92c4e1b63f54b81578115353de9fabbb2b607c0c
Jirapat Limpanich <jirapat.lpn@gmail.com>
2018-12-20 15:36:23 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -14,7 +14,10 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		
+		//if ($event['type'] == 'message' && $event['message']['type'] == 'text'){
+		if(strstr($event['message']['type'], 'สวัสดี'))
+		{
 			// Get text sent
 			$text = $event['source']['userId'];
 			// Get replyToken
@@ -23,7 +26,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "ฮัลโหลคิดตี้"
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
